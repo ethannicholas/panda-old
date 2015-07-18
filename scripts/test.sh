020599aca6e8f0b1ea2f7c2b318fa0414db8621e
Ethan Nicholas <ethan@ethannicholas.com>
2015-07-17 22:26:58 -0400

    bugfixes

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 export BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 cd $BASEDIR
 
-rm -f build/TestHarness.jar
-$PANDAC_JAVA -f jar -o build/TestHarness.jar src/test/TestHarness.panda
-java -jar build/TestHarness.jar --java $JAVA_HOME/bin/java "$PANDAC_JAVA" src build
+rm -f build/TestHarness
+$PANDAC -o build/TestHarness src/test/TestHarness.panda
+build/TestHarness "$PANDAC" src build
